224e546c902a2af05a5613e5e62fb1ddacaa6e94
Himanshu Pandey <himanshupandey7860988150@gmail.com>
2025-11-04 01:45:16 +0530

    Refactor to use Streamlit secrets for Google API key and model name

--- genai/01_project_01_project_text_summarizer_and_sentiment_analyzer/01_pchain_project_text_summarizer_and_sentiment_analyzer.py
+++ genai/01_project_01_project_text_summarizer_and_sentiment_analyzer/01_pchain_project_text_summarizer_and_sentiment_analyzer.py
@@ -1,6 +1,8 @@
 # from dotenv import load_dotenv
 
-import tomllib  # Python 3.11+; if older, pip install toml and use "import toml as tomllib"
+# import tomllib  # Python 3.11+; if older, pip install toml and use "import toml as tomllib"
+
+
 import os
 import streamlit as st
 
@@ -12,11 +14,10 @@ import streamlit as st
 # google_api_key = os.getenv("GOOGLE_API_KEY")
 
 
-# Load config.toml
-with open("config.toml", "rb") as f:
-    config = tomllib.load(f)
-
-google_api_key = config["google"]["GOOGLE_API_KEY"]
+# # Load config.toml
+# with open("config.toml", "rb") as f:
+#     config = tomllib.load(f)
+# google_api_key = config["google"]["GOOGLE_API_KEY"]
 
 
 from google import genai
@@ -28,9 +29,12 @@ st.title('Text Summarizer & Sentiment Analyzer')
 
 
 # os.environ['GOOGLE_API_KEY'] = google_api_key
-
 # genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 # client = genai.Client(api_key=os.environ['GOOGLE_API_KEY'])
+
+# Access secrets safely
+google_api_key = st.secrets["google"]["GOOGLE_API_KEY"]
+model_name = st.secrets["google"]["model"]
 
 client = genai.Client(api_key=google_api_key)
 
@@ -63,7 +67,8 @@ if st.button('Start Summary') :
     # response1 = model.generate_content(prompt1)
     resp1 = client.models.generate_content(
         # model="gemini-2.5-flash",
-        model="models/gemini-2.0-flash-001",
+        # model="models/gemini-2.0-flash-001",
+        model=model_name,
         contents=prompt1
     )
 
@@ -79,7 +84,8 @@ if st.button('Start Summary') :
     # response2 = model.generate_content(prompt2)
     resp2 = client.models.generate_content(
         # model="google-2.5-flash",
-        model="models/gemini-2.0-flash-001",
+        # model="models/gemini-2.0-flash-001",
+        model=model_name,
         contents=prompt2
     )
 
