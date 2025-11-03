2f90dae119269ca6827227e8a6086a78e1a3b2cc
Himanshu Pandey <himanshupandey7860988150@gmail.com>
2025-11-04 00:00:44 +0530

    Add text summarizer and sentiment analyzer using Streamlit and Google Generative AI

---
+++ genai/01_project_01_project_text_summarizer_and_sentiment_analyzer/01_pchain_project_text_summarizer_and_sentiment_analyzer.py
@@ -0,0 +1,83 @@
+from dotenv import load_dotenv
+import os
+
+# import google.generativeai as genai #pip install google-generativeai``
+import streamlit as st
+
+# Load the .env file
+load_dotenv()
+
+
+from google import genai
+from google.genai import types
+
+
+# st.title('Language Translation')
+st.title('Text Summarizer & Sentiment Analyzer')
+
+
+# Access environment variables
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+# os.environ['GOOGLE_API_KEY'] = google_api_key
+
+# genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
+# client = genai.Client(api_key=os.environ['GOOGLE_API_KEY'])
+
+client = genai.Client(api_key=google_api_key)
+
+
+# * Gemini API ke client.models.list() output. 😎 Yeh basically saare available Google Gemini aur allied models dikhata hai 
+# models_names = ""
+# for m in client.models.list():
+#     models_names = models_names  + ", " + m.name
+# st.write(models_names)
+
+user_input = st.text_area("Enter your content")
+
+
+# * Directly response ke time per kar lete hai
+# model = genai.GenerativeModel('gemini-pro')
+# model = genai.GenerativeModel('gemini-1.0-pro')
+# model = genai.GenerativeModel('gemini-1.5-pro-latest')
+
+
+if st.button('Start Summary') :
+    # Summarize prompt
+    prompt1 = f"""
+        You are an expert linguist, who is good at summarizing the content.
+        Help me summarizing the content into 2 lines.
+        ```
+        {user_input}
+        ```
+        """
+    
+    # response1 = model.generate_content(prompt1)
+    resp1 = client.models.generate_content(
+        # model="gemini-2.5-flash",
+        model="models/gemini-2.0-flash-001",
+        contents=prompt1
+    )
+
+    # Sentiment analysis prompt : # You need to analyze the sentiment of provided text as positive/negative.
+    prompt2 = f"""
+        Analyse the sentiment (Positive / Negative) of the following summary:
+
+        ```
+        {resp1.text} # This is the summary
+        ```
+    """
+
+    # response2 = model.generate_content(prompt2)
+    resp2 = client.models.generate_content(
+        # model="google-2.5-flash",
+        model="models/gemini-2.0-flash-001",
+        contents=prompt2
+    )
+
+
+    st.success(f"Summary: {resp1.text}")
+    st.success(f"Sentiment: {resp2.text}")
+
+
+
